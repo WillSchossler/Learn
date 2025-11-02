9f5152ced2ac8e8224d41aa7b4deb3db26b50f5f
William Gabriel Schossler <gabrielschossler.principal@gmail.com>
2025-11-02 15:47:10 -0300

    First exemple fastapi

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import os
-PATH = os.path.dirname(os.path.abspath(__file__))
-teste = os.path.abspath(".")
-print(PATH)
-print(teste)
+"""
+uvicorn main:app --reload
+"""
+import fastapi
+
+app = fastapi.FastAPI()
+iden = 5
+todos = [
+    {"id": 1, "description": "correr", "active": True},
+    {"id": 2, "description": "malhar", "active": True},
+    {"id": 3, "description": "viajar", "active": False},
+    {"id": 4, "description": "comprar", "active": True},
+]
+
+@app.get("/pegar")
+def pegar(identifica: int=None, ativo: bool=True):
+    for dic in todos:
+        if dic["id"] == identifica and dic["active"] == ativo:
+            return dic
+        
+@app.post("/inserir")
+def inserir(desc: str=None, ativo: bool=True):
+    global iden
+    try:
+        todos.append({"id": iden, "description": desc, "active": ativo})
+        iden += 1
+        return "Enviado com sucesso"
+    except:
+        return "Erro no envio"
